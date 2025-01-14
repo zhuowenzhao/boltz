38228ecde1df620935908d72e5960fa4a7ed3de5
Gabriele Corso <gabri.corso@gmail.com>
2025-01-13 16:08:23 -0800

    fix unused parameters structure training

--- src/boltz/model/model.py
+++ src/boltz/model/model.py
@@ -198,6 +198,7 @@ class Boltz1(LightningModule):
             )
 
         # Output modules
+        use_accumulate_token_repr = confidence_prediction and "use_s_diffusion" in confidence_model_args and confidence_model_args["use_s_diffusion"]
         self.structure_module = AtomDiffusion(
             score_model_args={
                 "token_z": token_z,
@@ -210,8 +211,7 @@ class Boltz1(LightningModule):
                 **score_model_args,
             },
             compile_score=compile_structure,
-            accumulate_token_repr="use_s_diffusion" in confidence_model_args
-            and confidence_model_args["use_s_diffusion"],
+            accumulate_token_repr=use_accumulate_token_repr
             **diffusion_process_args,
         )
         self.distogram_module = DistogramModule(token_z, num_bins)
