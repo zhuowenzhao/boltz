734cc2ee18190a27f546bacc096da38b8b0a46e9
Gabriele Corso <gabri.corso@gmail.com>
2025-05-01 19:03:05 -0400

    fix cpu version #246

--- src/boltz/main.py
+++ src/boltz/main.py
@@ -724,8 +724,8 @@ def predict(
     diffusion_params = BoltzDiffusionParams()
     diffusion_params.step_scale = step_scale
 
-    pairformer_args = PairformerArgs()
-    msa_module_args = MSAModuleArgs()
+    pairformer_args = PairformerArgs(use_trifast=(accelerator != "cpu"))
+    msa_module_args = MSAModuleArgs(use_trifast=(accelerator != "cpu"))
 
     steering_args = BoltzSteeringParams()
     if no_potentials:
